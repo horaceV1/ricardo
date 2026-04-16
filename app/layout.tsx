60b7d81e71a0ffcbb77ffd3df30bdf915ed65d91
horaceV1 <danielfilipep18@gmail.com>
2026-04-16 10:36:20 +0000

    feat: add Meta Pixel tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -191,6 +191,33 @@ export default function RootLayout({
           strategy="lazyOnload"
           src="https://chimpstatic.com/mcjs-connected/js/users/e0a7bc7769d6ab15c59d53f5e/9ccb812cd7e73802ff9c39e7f.js"
         />
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1137530448499869');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1137530448499869&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   )
